54f32ac1a21cae2e7b83b26d417da8b434b2ddee
atsutama2 <atsutama.0713@gmail.com>
2021-02-02 20:05:36 +0900

    Python test

--- main.py
+++ main.py
@@ -5,8 +5,22 @@
 
 
 def print_hi(name):
+    num = 1
+    flag = True
+    name = "12"
+    number: int = 1
+    user: str = "test"
+
     # Use a breakpoint in the code line below to debug your script.
-    print(f'Hi, {name}')  # Press ⌘F8 to toggle the breakpoint.
+    print(f'Hi, {name}', type(name))  # Press ⌘F8 to toggle the breakpoint.
+    print(num, type(num))
+    print(flag, type(flag))
+    print(int(name), type(int(name)))
+    print(number, type(number))
+    print(user, type(user))
+
+    print('ほげ', 'ほげ', sep=',', end='.\n')
+    print('ほげ', 'ほげ', sep=',', end='.\n')
 
 
 # Press the green button in the gutter to run the script.
