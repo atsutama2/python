a0c6de7c39d07b271cd1f951708f2609d7c5d87a
atsutama2 <atsutama.0713@gmail.com>
2021-02-02 11:35:32 +0900

    Python環境変更

--- scraping/tempCodeRunnerFile.py
+++
@@ -1,1 +0,0 @@
-#!/Users/fujitayuya/.pyenv/shims/python3
--- setup.sh
+++ setup.sh
@@ -1,5 +1,8 @@
 #!/bin/bash
 
 #git設定
-git config --local user.name "yuyafujita-eng"
-git config --local user.email "yuyafujita.eng@gmail.com"
+git config --local user.name "atsutama2"
+git config --local user.email "atsutama.0713@gmail.com"
+
+git remote set-url origin git@atsutama:atsutama2/python.git
+#git remote set-url origin git@github.com:atsutama2/python.git
