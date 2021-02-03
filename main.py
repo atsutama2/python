5eadc2de5772f2e3cee75367a86fab34b99ae3b7
atsutama2 <atsutama.0713@gmail.com>
2021-02-03 19:10:58 +0900

    test

--- main.py
+++ main.py
@@ -1,8 +1,4 @@
-# This is a sample Python script.
-
-# Press ⌃R to execute it or replace it with your code.
-# Press Double ⇧ to search everywhere for classes, files, tool windows, actions, and settings.
-
+import math
 
 def print_hi(name):
     num = 1
@@ -10,9 +6,10 @@ def print_hi(name):
     name = "12"
     number: int = 1
     user: str = "test"
+    pie = 17 / 3
+    result = math.sqrt(25)
 
-    # Use a breakpoint in the code line below to debug your script.
-    print(f'Hi, {name}', type(name))  # Press ⌘F8 to toggle the breakpoint.
+    print(f'Hi, {name}', type(name))
     print(num, type(num))
     print(flag, type(flag))
     print(int(name), type(int(name)))
@@ -22,9 +19,28 @@ def print_hi(name):
     print('ほげ', 'ほげ', sep=',', end='.\n')
     print('ほげ', 'ほげ', sep=',', end='.\n')
 
+    text = ('uuuuuuuuuuuuuuuuuuuuuuuuuuuu'
+           'ffffffffffffffffffffffffffff')
+    print(text)
 
-# Press the green button in the gutter to run the script.
+    print('どーも\n' * 10)
+    print('こんにちは.\nHow are you')
+    print(r'C:\proj\name')
+    print("""
+        あ
+    い
+        う
+    え
+        お
+    """)
+
+
+    print(pie)
+    print(17 // 3)
+    print(5*5*5*5*5)
+    print(5**5)
+    print(round(pie, 2))
+    print(result)
+
 if __name__ == '__main__':
     print_hi('Hello World')
-
-# See PyCharm help at https://www.jetbrains.com/help/pycharm/
